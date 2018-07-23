4f36c73a35490f919f958254c097aa9b75654dbb
Kenshin Abe <abe.kenshin@gmail.com>
2018-07-23 23:47:01 +0900

    y+1のときに角に戻らない

--- main.cpp
+++ main.cpp
@@ -71,8 +71,6 @@ int main() {
         }
       }
     }
-    cmd += move_from_to(curpos, {min_x, y + 1, min_z});
-    curpos = {min_x, y + 1, min_z};
     VI startpos = {min_x, y + 1, min_z};
     int xoff = 0;
     int zoff = 0;
@@ -82,6 +80,7 @@ int main() {
       if (model[startpos[0] + xoff][y][startpos[2] + zoff]) {
         cmd += long_move_s(curpos[0], startpos[0] + xoff, x_axis);
         cmd += long_move_s(curpos[2], startpos[2] + zoff, z_axis);
+        cmd += long_move_s(curpos[1], y + 1, y_axis);
         curpos = {startpos[0] + xoff, y + 1, startpos[2] + zoff};
         if (!is_high && query_ans[ind]) {
           cmd += void_s({0, -1, 0});
